18a6334c0467b050413aaaa65a065c64069f5993
HussamBarbour <free92.php@gmail.com>
2024-09-10 00:26:44 +0300

    User extends Authenticatable implements FilamentUser

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,8 +9,10 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
+use Filament\Panel;
+use Illuminate\Support\Facades\Auth;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasFactory, Notifiable;
     use HasRoles;
@@ -47,5 +49,10 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return Auth::user()->hasRole(['super_admin', 'panel_user']);
     }
 }
